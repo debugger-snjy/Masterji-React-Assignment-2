5cff2f86230508cb2ff4ae1a0c0e4816249096b5
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-08-16 18:03:11 +0530

    Completed the Cats Listing Page
    - Minor UI Fixes like fonts are remaining
    - Scrolling Data Loaded but Loading Not Showing !!
    - Added the no-scrollbar class tha will be help us to hide the scrollbar in the list

--- src/components/CatsCard.jsx
+++ src/components/CatsCard.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
-function CatsCard() {
+function CatsCard(props) {
     return (
-        <div>CatsCard</div>
+        <div class="max-w-md align-top inline-block mx-5 rounded-3xl overflow-hidden shadow-lg whitespace-normal">
+            <div class="w-full h-[400px] bg-gray-100 overflow-hidden">
+                <img src={props.catDetails.image} alt="Sunset in the mountains" class="w-full h-full object-cover" />
+            </div>
+            <div className='p-4 px-6 bg-white'>
+                <div class="">
+                    <div class="font-bold text-xl mb-2">{props.catDetails.name}</div>
+                    <p class="text-gray-700 text-base">
+                        {props.catDetails.description}
+                    </p>
+                </div>
+
+                <div className='flex text-lg font-DM mt-3'>
+                    <div className='w-1/3 font-bold italic'>Origin</div>
+                    <div className='w-2/3'>{props.catDetails.origin}</div>
+                </div>
+
+                <div className='flex flex-col text-lg font-DM mt-2  gap-2'>
+                    <div className='font-bold italic'>Temperament</div>
+                    <div className='tracking-tight'>
+                        {
+                            props.catDetails.temperament.split(',').map((temp) => {
+                                return <span class="inline-block bg-[#EFEFEF] hover:bg-[#D482DB] rounded-full px-3 py-1 text-base font-light text-[#353535] hover:text-white cursor-pointer mr-2 mb-2">{temp}</span>
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className='flex text-lg font-DM mt-3'>
+                    <div className='w-1/3 font-bold italic'>Life Span</div>
+                    <div className='w-2/3'>{props.catDetails.life_span} years</div>
+                </div>
+
+                <div className="mt-10 text-[#7D99E2] font-medium text-base">
+                    <a href={props.catDetails.wikipedia_url}>Learn More</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/CatListingPage.jsx
+++ src/pages/CatListingPage.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CatsCard from '../components/CatsCard'
+import { getCatsData } from "../api/getCatsLists.js"
 
 function CatListingPage() {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [catsData, setCatsData] = useState();
+    const [otherData, setOtherData] = useState();
+    const [page, setPage] = useState(1)
+
+    async function fetchData() {
+        setIsLoading(true);
+        console.log(page);
+
+        const data = await getCatsData(page, 5);
+        if(catsData){
+            var prevData = catsData;
+            prevData.push(...data.data.data)
+            setCatsData(prevData)
+        }
+        else{
+            setCatsData(data.data.data)
+        }
+        setOtherData(data.data)
+        console.log(data.data)
+        setIsLoading(false);
+    }
+
+    function checkScroll(e) {
+        const remainingWidth = e.target.scrollWidth - e.target.scrollLeft - e.target.clientWidth;
+        if (remainingWidth <= 500) {
+
+            if (otherData.nextPage) {
+                setPage((page) => page + 1)
+
+                // calling the function to load other data 
+                fetchData();
+            }
+            else{
+                alert("All Data Fetched !!")
+            }
+
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+        console.log(catsData);
+    }, [])
+
     return (
-        <div>CatListingPage</div>
+        <div className="p-9 catListingPage flex-col items-center justify-center h-full">
+            <div className="text-5xl mb-7 font-bold text-white">Cats around us</div>
+            <div className="horizontalScrolling overflow-x-auto overflow-y-hidden whitespace-nowrap no-scrollbar" onScroll={checkScroll}>
+                {console.log(catsData)}
+                {catsData && catsData.map((data) => (
+                    <CatsCard catDetails={data} key={data.id} />
+                ))}
+            </div>
+        </div>
     )
 }
 
